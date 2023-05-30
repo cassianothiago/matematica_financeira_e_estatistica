b8cef26d30eaf85881003b46ca13a176fff780a8
cassianothiago <cassianothiago@rocketmail.com>
2023-05-30 14:36:12 -0300

    Update 2.mat-f-regra da sociedade.py

---
+++ 2.mat-f-regra da sociedade.py
@@ -0,0 +1,19 @@
+import os
+
+print('='*120)
+print('Regra da sociedade:.')
+print('modelo matemático de distribuição de resultado:.')
+print('='*120)
+print('..Menbros da sociedade(M)..')
+print('  Participantes da sociedade')
+print('..Tempo(t)..')
+print('  tempo de participação de cada menbro na sociedade')
+print('..Peso(P)..')
+print('  proporção do capital entre os membros')
+print('..Capital total(C)..')
+print('  total de capital de cada participante.')
+print('='*120)
+
+print('Formulação básica')
+print('P = C x T')
+
